9edd10237f0c325948acbcb54422ba55b0b6d577
Adam Smith <adamsmith@algorithmiv.com>
2016-01-21 16:16:08 -0800

    Added dummy unit tests

---
+++ test/tests.js
@@ -0,0 +1,34 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT UNIT TESTS
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+describe('reporter tests', function() {
+
+  it('test pass', function() {
+    assert(true);
+  });
+
+  it('test assert fail', function() {
+    assert(false);
+  });
+
+  it('test error thrown', function() {
+    throw new TypeError('test');
+  });
+
+});
