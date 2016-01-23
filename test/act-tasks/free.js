b8b4a3756f652e80c4c0fabe75308f2d0f5886c8
Adam Smith <adamsmith@algorithmiv.com>
2016-01-23 02:55:31 -0800

    Added the test task: free

---
+++ test/act-tasks/free.js
@@ -0,0 +1,29 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TEST TASK: free
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+exports['desc'] = 'change your lens';
+exports['method'] = liveFree;
+
+/**
+ * @public
+ * @type {function}
+ */
+function liveFree() {
+  fuse(actOut, 'free');
+}
