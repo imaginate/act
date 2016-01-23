94b3842db73bae25c5af628b81d0839aec8794a9
Adam Smith <adamsmith@algorithmiv.com>
2016-01-23 03:37:02 -0800

    Updated version to 1.0.1

--- src/show-help.js
+++ src/show-help.js
@@ -2,7 +2,7 @@
  * -----------------------------------------------------------------------------
  * ACT: SHOW-HELP
  * -----------------------------------------------------------------------------
- * @version 1.0.0
+ * @version 1.0.1
  * @see [act]{@link https://github.com/imaginate/act}
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
