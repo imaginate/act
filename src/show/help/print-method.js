9ccd4951f70a2694f3351c609e648db1cfc4cfca
Adam Smith <adamsmith@algorithmiv.com>
2016-01-24 22:46:12 -0800

    Added showHelp method: printHelpMethod

---
+++ src/show/help/print-method.js
@@ -0,0 +1,47 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT: PRINT-HELP-METHOD
+ * -----------------------------------------------------------------------------
+ * @version 1.0.1
+ * @see [act]{@link https://github.com/imaginate/act}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var help = require('../../helpers');
+var fill = help.fill;
+var fuse = help.fuse;
+
+/**
+ * @param {?HelpMethod} method
+ * @param {number} len
+ * @return {string}
+ */
+module.exports = function printHelpMethod(method, len) {
+
+  /** @type {string} */
+  var space;
+  /** @type {string} */
+  var name;
+  /** @type {string} */
+  var val;
+
+  if (!method) return '';
+
+  val = method.val && fuse('= <', method.val, '>')
+  name = fuse(method.name, val);
+  space = fill(len - name.length, ' ');
+  return fuse('      -', name, space, method.desc, '\n');
+};
