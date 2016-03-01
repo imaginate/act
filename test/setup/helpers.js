41855df82ba54a0d3d0b6861d14b6e3678bff7c5
Adam Smith <adamsmith@algorithmiv.com>
2016-03-01 00:54:23 -0800

    Added new tests helpers

--- test/setup/helpers.js
+++ test/setup/helpers.js
@@ -1,12 +1,11 @@
 /**
  * -----------------------------------------------------------------------------
- * ACT UNIT TESTS: SETUP
+ * ACT UNIT TESTS SETUP: helpers
  * -----------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
  * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  * @see [vitals]{@link https://github.com/imaginate/vitals}
  * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
  *
@@ -17,21 +16,18 @@
 
 'use strict';
 
-process.chdir('test');
-
 global.assert = require('assert');
-global.act = require('../act');
+global.act = require('../../act.js');
+global.log = require('log-ocd')();
 
-// globally append all of are, vitals, and log-ocd methods
-require('node-are')();
-require('node-vitals')(2, 'all');
-require('log-ocd')('log');
+require('node-vitals')('base', 'fs').mkGlobal();
 
 log.error.setConfig({
   'logger': logError,
   'throw':  false,
   'exit':   false
 });
+
 log.error.setFormat({
   'linesAfter': 2
 });
@@ -44,6 +40,7 @@ log.fail.setFormat({
     'accentMark': ''
   }
 });
+
 log.fail.setStyle({
   'header': {
     'color': 'red',
