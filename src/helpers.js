feb3decb8bd3407891501a3957fe73e4070f2460
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 22:52:15 -0800

    Updated the src helpers

--- src/helpers.js
+++ src/helpers.js
@@ -9,7 +9,6 @@
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
  * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  * @see [vitals]{@link https://github.com/imaginate/vitals}
  * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
  *
@@ -20,28 +19,21 @@
 
 'use strict';
 
-// see https://github.com/imaginate/are
-var are = require('node-are');
-exports.is  = are.is;
-exports.are = are.are;
-
 // see https://github.com/imaginate/vitals
-var vitals = require('node-vitals')('all');
-exports.amend  = vitals.amend;
+var vitals = require('node-vitals')('base', 'fs');
 exports.copy   = vitals.copy;
-exports.create = vitals.create;
 exports.cut    = vitals.cut;
 exports.each   = vitals.each;
 exports.fill   = vitals.fill;
-exports.freeze = vitals.freeze;
 exports.fuse   = vitals.fuse;
 exports.get    = vitals.get;
 exports.has    = vitals.has;
+exports.is     = vitals.is;
 exports.remap  = vitals.remap;
 exports.roll   = vitals.roll;
-exports.run    = vitals.run;
-exports.seal   = vitals.seal;
+exports.same   = vitals.same;
 exports.slice  = vitals.slice;
+exports.to     = vitals.to;
 exports.until  = vitals.until;
 
 // see https://github.com/imaginate/log-ocd
@@ -49,8 +41,8 @@ var log = require('log-ocd')();
 
 log.error.setConfig({
   'ocdmap': true,
-  'throw': false,
-  'exit':  true
+  'throw':  false,
+  'exit':   true
 });
 
 log.error.setFormat({
