47bceb5148edb67a99afc336555742c4c6e96cc9
Adam Smith <imagineadamsmith@gmail.com>
2022-12-14 23:42:06 -0800

    Rebuilt version task

--- tasks/version.js
+++ tasks/version.js
@@ -2,120 +2,115 @@
  * -----------------------------------------------------------------------------
  * LOCAL ACT TASK: version
  * -----------------------------------------------------------------------------
- * @file Use `$ node bin/act version` to access this file.
+ * @file Use `act version` to access this file.
  * @version 1.4.1
  *
- * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
- * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
  *
- * @see [JSDoc3](http://usejsdoc.org)
+ * @see [JSDoc3](https://jsdoc.app)
  * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ * @see [Node Path](https://nodejs.org/dist/latest-v14.x/docs/api/path.html)
+ * @see [Vitals](https://github.com/imaginate/vitals)
  */
 
 'use strict';
 
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 // EXPORTS
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
-exports['desc'] = 'updates version for the repo';
-exports['value'] = 'x.x.x-pre.x';
-exports['default'] = '-all';
-exports['methods'] = {
-  'all': {
-    'desc': 'updates version for entire repo',
-    'value': 'x.x.x-pre.x',
-    'method': updateAllVersion
-  },
-  'npm': {
-    'desc': 'updates only npm version',
-    'value': 'x.x.x-pre.x',
-    'method': updateNPMVersion
-  }
+exports.desc = 'updates version for the repo';
+exports.value = 'x.x.x-pre.x';
+exports.default = '-all';
+exports.methods = {
+    all: {
+        desc: 'updates version for entire repo',
+        value: 'x.x.x-pre.x',
+        method: updateAllVersion
+    },
+    npm: {
+        desc: 'updates only npm version',
+        value: 'x.x.x-pre.x',
+        method: updateNpmVersion
+    }
 };
 
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 // HELPERS
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
-var vitals = require('node-vitals')('base', 'fs');
-var each   = vitals.each;
-var fuse   = vitals.fuse;
-var get    = vitals.get;
-var has    = vitals.has;
-var remap  = vitals.remap;
-var to     = vitals.to;
+const {
+    each,
+    fuse,
+    get,
+    has,
+    remap,
+    to
+} = require('node-vitals')('base', 'fs');
 
-var path = require('path');
-var resolve = path.resolve;
+const { resolve } = require('path');
 
-////////////////////////////////////////////////////////////
-// CONSTANTS
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+// PATHS
+//////////////////////////////////////////////////////////////////////////////
 
-var ROOTPATH = resolve(__dirname, '..');
-var ERRMSG   = 'invalid value (must be a semantic version)';
+const ROOT_DIRPATH = resolve(__dirname, '..');
 
-var SEMVER = /^[0-9]+\.[0-9]+\.[0-9]+(?:-[a-z]+\.?[0-9]*)?$/;
-var BDGVER = /(badge\/npm-)[0-9]+\.[0-9]+\.[0-9]+(?:--[a-z]+\.?[0-9]*)?/;
-var SRCVER = /\b(v?)[0-9]+\.[0-9]+\.[0-9]+(?:-[a-z]+\.?[0-9]*)?\b/g;
-var PKGVER = /("version": ")[0-9]+\.[0-9]+\.[0-9]+(?:-[a-z]+\.?[0-9]*)?/;
+//////////////////////////////////////////////////////////////////////////////
+// PATTERNS
+//////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////
+const SEMVER_PATT = /^[0-9]+\.[0-9]+\.[0-9]+(?:-[a-z]+(?:\.[0-9]+)?)?$/;
+const BDGVER_PATT =
+    /(?<=badge\/npm-)[0-9]+\.[0-9]+\.[0-9]+(?:--[a-z]+(?:\.[0-9]+)?)?/;
+const SRCVER_PATT =
+    /(?<=\bv?)[0-9]+\.[0-9]+\.[0-9]+(?:-[a-z]+(?:\.[0-9]+)?)?\b/g;
+const PKGVER_PATT =
+    /(?<="version": ")[0-9]+\.[0-9]+\.[0-9]+(?:-[a-z]+(?:\.[0-9]+)?)?(?=")/;
+
+//////////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 /**
  * @public
  * @param {string} version
+ * @return {void}
  */
 function updateAllVersion(version) {
 
-  /** @type {!Array<string>} */
-  var filepaths;
+    if (!isSemVersion(version)) {
+        throw new Error(fuse(
+            'invalid version `', version, "' (must be a semantic version)"
+        ));
+    }
 
-  if ( !isSemVersion(version) ) throw new Error(ERRMSG);
-
-  filepaths = get.filepaths(ROOTPATH, {
-    basepath:    true,
-    recursive:   true,
-    validExts:   'js',
-    invalidDirs: '.*|node_modules|tmp'
-  });
-  insertVersions(filepaths, version);
-
-  updateNPMVersion(version);
+    updateJs(version);
+    updatePackage(version);
+    updateReadme(version);
 }
 
 /**
  * @public
  * @param {string} version
+ * @return {void}
  */
-function updateNPMVersion(version) {
+function updateNpmVersion(version) {
 
-  /** @type {string} */
-  var filepath;
-  /** @type {string} */
-  var content;
+    if (!isSemVersion(version)) {
+        throw new Error(fuse(
+            'invalid version `', version, "' (must be a semantic version)"
+        ));
+    }
 
-  if ( !isSemVersion(version) ) throw new Error(ERRMSG);
-
-  filepath = resolve(ROOTPATH, 'package.json');
-  content  = get.file(filepath);
-  version  = fuse('$1', version);
-  content  = remap(content, PKGVER, version);
-  to.file(content, filepath);
-
-  filepath = resolve(ROOTPATH, 'README.md');
-  content  = get.file(filepath);
-  version  = remap(version, /-/, '--');
-  content  = remap(content, BDGVER, version);
-  to.file(content, filepath);
+    updatePackage(version);
+    updateReadme(version);
 }
 
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 // PRIVATE METHODS
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 /**
  * @private
@@ -123,33 +118,69 @@ function updateNPMVersion(version) {
  * @return {boolean}
  */
 function isSemVersion(version) {
-  return !!version && has(version, SEMVER);
+    return !!version && has(version, SEMVER_PATT);
 }
 
 /**
  * @private
- * @param {!Array<string>} filepaths
  * @param {string} version
+ * @return {void}
  */
-function insertVersions(filepaths, version) {
-  version = fuse('$1', version);
-  each(filepaths, function(filepath) {
-    insertVersion(filepath, version);
-  });
+function updateJs(version) {
+    /** @const {!Array<string>} */
+    const filepaths = get.filepaths(ROOT_DIRPATH, {
+        basepath: true,
+        recursive: true,
+        validExts: 'js',
+        invalidDirs: '.*|node_modules|tmp'
+    });
+    each(filepaths, (filepath) => {
+        updateJsFile(filepath, version);
+    });
 }
 
 /**
  * @private
  * @param {string} filepath
  * @param {string} version
+ * @return {void}
  */
-function insertVersion(filepath, version) {
-
-  /** @type {string} */
-  var content;
-
-  content = get.file(filepath);
-  content = remap(content, SRCVER, version);
-  to.file(content, filepath);
+function updateJsFile(filepath, version) {
+    /** @const {string} */
+    const content = get.file(filepath);
+    /** @const {string} */
+    const result = remap(content, SRCVER_PATT, version);
+    to.file(result, filepath);
 }
 
+/**
+ * @private
+ * @param {string} version
+ * @return {void}
+ */
+function updatePackage(version) {
+    /** @const {string} */
+    const filepath = resolve(ROOT_DIRPATH, 'package.json');
+    /** @const {string} */
+    const content = get.file(filepath);
+    /** @const {string} */
+    const result = remap(content, PKGVER_PATT, version);
+    to.file(result, filepath);
+}
+
+/**
+ * @private
+ * @param {string} version
+ * @return {void}
+ */
+function updateReadme(version) {
+    /** @const {string} */
+    const filepath = resolve(ROOT_DIRPATH, 'README.md');
+    /** @const {string} */
+    const content = get.file(filepath);
+    /** @const {string} */
+    const replace = remap(version, /-/, '--');
+    /** @const {string} */
+    const result = remap(content, BDGVER_PATT, replace);
+    to.file(result, filepath);
+}
