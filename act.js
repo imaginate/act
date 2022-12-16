7616ba1a964785f4e97095cf419adb1f54edcb1b
Adam Smith <imagineadamsmith@gmail.com>
2022-12-15 22:36:59 -0800

    Reformatted the main act file

--- act.js
+++ act.js
@@ -20,33 +20,30 @@
 'use strict';
 
 // save reference to the base path
-var BASE_DIR = process.cwd();
+const BASE_DIR = process.cwd();
 
-var help = require('./src/helpers');
-var cut   = help.cut;
-var has   = help.has;
-var is    = help.is;
-var log   = help.log;
-var slice = help.slice;
+const { cut, has, is, log, slice } = require('./src/helpers');
 
 /**
  * @typedef {!Array<string>} Args
  */
 
-/** @type {!RegExp} */
-var TRIM_START = /^ *(?:act +)?/;
-/** @type {!RegExp} */
-var TRIM_END = / +$/;
-/** @type {!RegExp} */
-var NODE = /^["']?(?:.+[\/\\])?node(?:.[a-z]+)?["']?$/;
-/** @type {!RegExp} */
-var ACT = /^["']?(?:.+[\/\\])?act(?:.js)?["']?$/;
+/** @const {!RegExp} */
+const TRIM_START = /^ *(?:act +)?/;
+/** @const {!RegExp} */
+const TRIM_END = / +$/;
+/** @const {!RegExp} */
+const NODE = /^["']?(?:.+[\/\\])?node(?:.[a-z]+)?["']?$/;
+/** @const {!RegExp} */
+const ACT = /^["']?(?:.+[\/\\])?act(?:.js)?["']?$/;
+/** @const {!RegExp} */
+const SPACE = / +/;
 
-var findTaskDir = require('./src/tasks/find-dir');
-var loadConfig  = require('./src/config/load');
-var showHelp    = require('./src/show/help');
-var showVersion = require('./src/show/version');
-var runTasks    = require('./src/tasks/run');
+const findTaskDir = require('./src/tasks/find-dir');
+const loadConfig = require('./src/config/load');
+const showHelp = require('./src/show/help');
+const showVersion = require('./src/show/version');
+const runTasks = require('./src/tasks/run');
 
 /**
  * @public
@@ -55,41 +52,37 @@ var runTasks    = require('./src/tasks/run');
  */
 module.exports = function initAct(cmd) {
 
-  /** @type {(?Config|boolean)} */
-  var config;
-  /** @type {!TypeError} */
-  var error;
-  /** @type {Args} */
-  var args;
-  /** @type {string} */
-  var dir;
+    /** @type {Args} */
+    let args;
 
-  if ( is.str(cmd) ) {
-    cmd = cut(cmd, TRIM_START);
-    cmd = cut(cmd, TRIM_END);
-    args = cmd.split(/ +/);
-  }
-  else if ( is('!strings', cmd) ) {
-    args = has(cmd[0], NODE) && has(cmd[1], ACT)
-      ? slice(cmd, 2)
-      : has(cmd[0], ACT)
-        ? slice(cmd, 1)
-        : cmd;
-  }
-  else {
-    error = 'invalid `cmd` param (must be a string or an array of strings)';
-    error = new TypeError(error);
-    log.error('Failed `act` command', error, { cmd: cmd });
-    return false;
-  }
+    if (is.str(cmd)) {
+        cmd = cut(cmd, TRIM_START);
+        cmd = cut(cmd, TRIM_END);
+        args = cmd.split(SPACE);
+    } else if (is('!strings', cmd)) {
+        args = has(cmd[0], NODE) && has(cmd[1], ACT)
+            ? slice(cmd, 2)
+            : has(cmd[0], ACT)
+                ? slice(cmd, 1)
+                : cmd;
+    } else {
+        /** @const {!TypeError} */
+        const error = new TypeError(
+            "invalid `cmd' param (must be a string or an array of strings)"
+        );
+        log.error("Failed `act' command.", error, { cmd: cmd });
+        return false;
+    }
 
-  dir = findTaskDir(BASE_DIR);
-  config = loadConfig(dir);
-  return config
-    ? is.help(args)
-      ? showHelp(dir, config)
-      : is.version(args)
-        ? showVersion(args)
-        : runTasks(dir, config, args)
-    : false;
+    /** @const {string} */
+    const dir = findTaskDir(BASE_DIR);
+    /** @const {(?Config|boolean)} */
+    const config = loadConfig(dir);
+    return config
+        ? is.help(args)
+            ? showHelp(dir, config)
+            : is.version(args)
+                ? showVersion(args)
+                : runTasks(dir, config, args)
+        : false;
 };
