f0812888e64505f265c8aa92230892a3b4ab68e6
Adam Smith <imagineadamsmith@gmail.com>
2022-12-15 22:42:24 -0800

    Reformatted, added "resolve", & updated "help" & "version" regexps in helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,76 +1,108 @@
 /**
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
  * ACT: HELPERS
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
  * @version 1.4.2
- * @see [act]{@link https://github.com/imaginate/act}
+ * @see [Act](https://github.com/imaginate/act)
  *
- * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
- * @copyright 2022 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
  *
  * Supporting Libraries:
- * @see [vitals]{@link https://github.com/imaginate/vitals}
- * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ * @see [Vitals](https://github.com/imaginate/vitals)
+ * @see [LogOCD](https://github.com/imaginate/log-ocd)
  *
  * Annotations:
- * @see [JSDoc3]{@link http://usejsdoc.org/}
- * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
 'use strict';
 
-// see https://github.com/imaginate/vitals
-var vitals = require('node-vitals')('base', 'fs');
-exports.copy   = vitals.copy;
-exports.cut    = vitals.cut;
-exports.each   = vitals.each;
-exports.fill   = vitals.fill;
-exports.fuse   = vitals.fuse;
-exports.get    = vitals.get;
-exports.has    = vitals.has;
-exports.is     = vitals.is;
-exports.remap  = vitals.remap;
-exports.roll   = vitals.roll;
-exports.same   = vitals.same;
-exports.slice  = vitals.slice;
-exports.to     = vitals.to;
-exports.until  = vitals.until;
+//////////////////////////////////////////////////////////////////////////////
+// VITALS
+//////////////////////////////////////////////////////////////////////////////
 
-// see https://github.com/imaginate/log-ocd
-var log = require('log-ocd')();
+const {
+    copy,
+    cut,
+    each,
+    fill,
+    fuse,
+    get,
+    has,
+    is,
+    remap,
+    roll,
+    same,
+    slice,
+    to,
+    until
+} = require('node-vitals')('base', 'fs');
+exports.copy = copy;
+exports.cut = cut;
+exports.each = each;
+exports.fill = fill;
+exports.fuse = fuse;
+exports.get = get;
+exports.has = has;
+exports.is = is;
+exports.remap = remap;
+exports.roll = roll;
+exports.same = same;
+exports.slice = slice;
+exports.to = to;
+exports.until = until;
 
+//////////////////////////////////////////////////////////////////////////////
+// LOG
+//////////////////////////////////////////////////////////////////////////////
+
+const log = require('log-ocd')();
 log.error.setConfig({
-  'ocdmap': true,
-  'throw':  false,
-  'exit':   true
+    ocdmap: true,
+    throw:  false,
+    exit:   true
 });
-
 log.error.setFormat({
-  'lineLimit': 80
+    lineLimit: 80
 });
-
 log.pass.setFormat({
-  'linesAfter': 0
+    linesAfter: 0
 });
-
 exports.log = log;
 
 //////////////////////////////////////////////////////////////////////////////
+// PATH
+//////////////////////////////////////////////////////////////////////////////
+
+const { resolve: _resolve } = require('path');
+
+/**
+ * @param {...string} path
+ * @return {string}
+ */
+exports.resolve = function resolve(...paths) {
+    /** @const {string} */
+    const path = _resolve(...paths);
+    return remap(path, /\\/g, '/');
+};
+
+//////////////////////////////////////////////////////////////////////////////
 // CUSTOM IS METHODS
-
-var has = vitals.has;
+//////////////////////////////////////////////////////////////////////////////
 
 /** @type {!RegExp} */
-var VERSION = /^-+v(?:ersion)?$/;
+const VERSION = /^--?v(?:ersion)?$/;
 /** @type {!RegExp} */
-var HELP = /^\?|-+h(?:elp)?$/;
+const HELP = /^(?:-?-?\?|--?h(?:elp)?)$/;
 
 /**
  * @param {Args} args
  * @return {boolean}
  */
 exports.is.help = function isHelp(args) {
-  return !args.length || has(args[0], HELP);
+    return !args.length || has(args[0], HELP);
 };
 
 /**
@@ -78,5 +110,5 @@ exports.is.help = function isHelp(args) {
  * @return {boolean}
  */
 exports.is.version = function isVersion(args) {
-  return has(args[0], VERSION);
+    return has(args[0], VERSION);
 };
